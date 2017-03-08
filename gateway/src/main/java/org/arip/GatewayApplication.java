d0114f7d5d1cca08df653b552785cdb6101729c8
Arip Hidayat <ariphidayat@hotmail.com>
2017-03-08 13:06:56 +0700

    Routing and Filtering using Netflix Zuul

---
+++ gateway/src/main/java/org/arip/GatewayApplication.java
@@ -0,0 +1,21 @@
+package org.arip;
+
+import org.arip.filters.SimpleZuulFilter;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
+import org.springframework.context.annotation.Bean;
+
+@EnableZuulProxy
+@SpringBootApplication
+public class GatewayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GatewayApplication.class, args);
+	}
+
+	@Bean
+	SimpleZuulFilter simpleZuulFilter() {
+		return new SimpleZuulFilter();
+	}
+}
