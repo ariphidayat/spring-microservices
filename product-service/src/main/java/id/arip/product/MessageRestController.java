43202119e1491d1eb214324de4396fbd0b80c2ea
Arip Hidayat <ariphidayat@hotmail.com>
2018-11-14 09:45:05 +0700

    Spring Cloud Config Server

---
+++ product-service/src/main/java/id/arip/product/MessageRestController.java
@@ -0,0 +1,22 @@
+package id.arip.product;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * Created by Arip Hidayat on 11/13/2018.
+ */
+@RefreshScope
+@RestController
+public class MessageRestController {
+
+    @Value("${message: Hello Default}")
+    private String message;
+
+    @RequestMapping("/message")
+    String getMessage() {
+        return this.message;
+    }
+}
